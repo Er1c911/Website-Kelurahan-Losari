83887bf3e299577c1781569c6cdabdebb215d213
unknown <fausta_ubraw@student.ub.ac.id>
2026-06-26 20:13:34 +0700

    fix: use /tmp runtime cache paths on Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('VERCEL')) {
+            $tmpFramework = '/tmp/framework';
+            $tmpViews = $tmpFramework.'/views';
+            $tmpCache = $tmpFramework.'/cache';
+
+            File::ensureDirectoryExists($tmpViews);
+            File::ensureDirectoryExists($tmpCache);
+
+            config([
+                'view.compiled' => $tmpViews,
+                'cache.stores.file.path' => $tmpCache,
+                'cache.stores.file.lock_path' => $tmpCache,
+            ]);
+        }
     }
 }
