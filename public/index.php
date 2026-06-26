2baceb9cbaffc289041dcc26c56175a3b8c1f5ab
unknown <fausta_ubraw@student.ub.ac.id>
2026-06-26 20:21:24 +0700

    fix: set Laravel runtime cache paths for Vercel in entrypoint

--- public/index.php
+++ public/index.php
@@ -5,6 +5,26 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if (getenv('VERCEL')) {
+    @mkdir('/tmp/bootstrap/cache', 0777, true);
+    @mkdir('/tmp/framework/views', 0777, true);
+
+    $runtimePaths = [
+        'APP_CONFIG_CACHE' => '/tmp/bootstrap/cache/config.php',
+        'APP_EVENTS_CACHE' => '/tmp/bootstrap/cache/events.php',
+        'APP_PACKAGES_CACHE' => '/tmp/bootstrap/cache/packages.php',
+        'APP_ROUTES_CACHE' => '/tmp/bootstrap/cache/routes.php',
+        'APP_SERVICES_CACHE' => '/tmp/bootstrap/cache/services.php',
+        'VIEW_COMPILED_PATH' => '/tmp/framework/views',
+    ];
+
+    foreach ($runtimePaths as $key => $value) {
+        putenv($key.'='.$value);
+        $_ENV[$key] = $value;
+        $_SERVER[$key] = $value;
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
